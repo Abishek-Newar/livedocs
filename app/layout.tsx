4cc7fac05b5c2e69565e10bb75c6bc7e26005af5
abishejk1872 <abisheknewar15@gmail.com>
2024-07-23 03:40:01 +0530

    changes

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import "./globals.css"
+import { Inter as FontSans } from "next/font/google"
+
+import { cn } from "@/lib/utils"
+import { Metadata } from "next"
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+export const metadata: Metadata = {
+  title: "SyncWrite",
+  description: "your go-to collaborative text editor"
+}
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
