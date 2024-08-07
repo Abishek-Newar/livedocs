0d97613f31df8dff6e6501012a334dd8e4caed0f
abishejk1872 <abisheknewar15@gmail.com>
2024-08-07 18:05:31 +0530

    changes

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -1,19 +1,47 @@
 "use client"
 import { ClientSideSuspense, RoomProvider } from '@liveblocks/react'
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { Editor } from '@/components/editor/Editor'
 import Navbar from '@/components/Navbar'
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 import ActiveCollaborators from './ActiveCollaborators'
+import { Input } from './ui/input'
+
 
 const CollaborativeRoom = ({roomId, roomMetadata}: CollaborativeRoomProps) => {
+    const [documentTitle,setDocumentTitle] = useState();
+    const [editing, setEditing] = useState();
+    const [loading, setLoading] = useState();
+
+    const containerRef = useRef<HTMLDivElement>(null)
+    const inputRef = useRef<HTMLDivElement>(null)
+
+    const updateTitleHandler = (e: React.KeyboardEvent<HTMLInputElement> ) =>{
+
+
+    }
     return (
         <RoomProvider id={roomId}>
             <ClientSideSuspense fallback={<div>Loading…</div>}>
                 <div className="collaborative-room">
                     <Navbar>
-                        <div className='flex w-fit items-center justify-center gap-2'>
-                            <p className='document-title'>Share</p>
+                        <div ref={containerRef} className='flex w-full items-center justify-center gap-2'>
+                           { editing && !loading?(
+                            <Input 
+                            type='text'
+                            value={documentTitle}
+                            ref={inputRef}
+                            placeholder="Enter title"
+                            onChange={(e)=> setDocumentTitle(e.target.value)}
+                            onKeyDown={updateTitleHandler}
+                            disable={!editing}
+                            className='document-title-page'
+                            />
+                           ):(
+                            <>
+                             <p className='document-title'>{documentTitle}</p>
+                            </>
+                           )}
                         </div>
                         <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
                             <ActiveCollaborators />
