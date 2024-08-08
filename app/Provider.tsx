491e2d099faae752d0cac1b23fcefb1d6da65985
abishejk1872 <abisheknewar15@gmail.com>
2024-08-08 21:37:51 +0530

    added user mention feature

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,9 +1,12 @@
 "use client"
 import Loader from '@/components/Loader'
 import { getClerkUsers, getDocumentUsers } from '@/lib/actions/user.actions'
+import { useUser } from '@clerk/nextjs'
 import {LiveblocksProvider, ClientSideSuspense} from '@liveblocks/react'
 import { ReactNode } from 'react'
 const Provider = ({children}: {children: ReactNode}) => {
+
+  const {user: clerkUser} = useUser();
   return (
     <LiveblocksProvider authEndpoint="/api/liveblocks-auth" resolveUsers={async ({userIds})=>{
       const users = await getClerkUsers({userIds});
